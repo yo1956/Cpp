f0777eba213a9b36cfa5c33cb254324be8b4da39
yo1956 <hm.hr.yossy@gmail.com>
2021-05-17 20:52:27 +0900

    ABC198A

---
+++ ABC198A-Div.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, n)      for (int i = 0; i < (int)(n); ++i)
+#define rep_e(i, n)    for (int i = 0; i <= (int)(n); ++i)
+#define rep1(i, n)     for (int i = 1; i < (int)(n); ++i)
+#define rep1_e(i, n)   for (int i = 1; i <= (int)(n); ++i)
+#define rep_s(i, s, n) for (int i = s; i < (int)(n); ++i)
+#define rep_v(i, v)    for (int i = 0; i < (int)(v.size()); ++i)
+#define repll(i, n)    for (ll i = 0; i < (ll)(n); ++i)
+
+#define MOD 1000000007
+
+template <typename T> bool chmax(T& a, const T& b); //aよりもbが大きいならばaをbで更新する //更新されたならばtrueを返す
+template <typename T> bool chmin(T& a, const T& b); //aよりもbが小さいならばaをbで更新する //更新されたならばtrueを返す
+
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	cout << n - 1 << endl; //(n-2) + 1
+
+	return 0;
+
+}
+
+
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T& a, const T& b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
