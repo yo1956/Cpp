e6914271f41bce27010f5628ed4d0545a76fd49b
yo1956 <hm.hr.yossy@gmail.com>
2021-05-28 17:39:28 +0900

    1day1ac準備

---
+++ ABC188A-Three-Point_Shot.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+#define rep(i, n)      for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)     for (int i = 1; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
+template <typename T> bool chmax(T& a, const T& b); //aよりもbが大きいならばaをbで更新する //更新されたならばtrueを返す
+template <typename T> bool chmin(T& a, const T& b); //aよりもbが小さいならばaをbで更新する //更新されたならばtrueを返す
+
+
+int main() {
+
+	int x, y;
+	cin >> x >> y;
+	
+	if(abs(x-y) < 3)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+
+	return 0;
+
+}
+
+
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T& a, const T& b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
