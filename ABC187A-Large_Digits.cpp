dd44143c22758c20cf3556dce0359779366f36b3
yo1956 <hm.hr.yossy@gmail.com>
2021-05-31 19:53:10 +0900

    1day1ac

--- ABC187A-Large_Digits.cpp
+++ ABC187A-Large_Digits.cpp
@@ -8,49 +8,19 @@ typedef long long ll;
 template <typename T> bool chmax(T& a, const T& b); //aよりもbが大きいならばaをbで更新する //更新されたならばtrueを返す
 template <typename T> bool chmin(T& a, const T& b); //aよりもbが小さいならばaをbで更新する //更新されたならばtrueを返す
 
+//各桁の和を求める
+int sum_digit(int n){
+   if(n < 10)  return n;
+   return sum_digit(n/10) + n%10;
+}
 
 int main() {
 
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	vector<vector<pair<int, int>>> xy(n);
-	rep(i,n){
-		cin >> a[i];
-		rep(j, a[i]){
-			int x, y;
-			cin >> x >> y;
-			x--;
-			xy[i].push_back(make_pair(x, y));
-		}
-	}
+	int a, b;
+	cin >> a >> b;
 
 	int ans = 0;
-	rep(bit, (1 << n)){
-		bool ok = true;
-		int count_honest = 0;
-		rep(i, n){
-			if(bit & (1 << i)){ //正直者と仮定している人について調べる
-				count_honest++;
-				rep(j, a[i]){  //正直者と仮定した人の証言を1つずつ調べる
-					if(xy[i][j].second == 1){ //正直者と仮定した人が正直者と言っている相手に、不親切な人と言われているなら矛盾
-						int honest = xy[i][j].first; //正直者と仮定した人が正直者と言っている相手
-						rep(k,a[honest]){
-							if((xy[honest][k].first == i) && (xy[honest][k].second == 0)){
-								ok = false;
-								break;
-							}
-						}
-					}
-					if(ok == false)
-						break;
-				}
-			}
-			if(ok == false)
-				break;
-		}
-		chmax(ans, count_honest);
-	}
+	ans = max(sum_digit(a), sum_digit(b));
 
 	cout << ans << endl;
 
