b7966a400f073f1a750a7d567ed8c00dfa1841ac
yo1956 <hm.hr.yossy@gmail.com>
2021-06-09 21:04:09 +0900

    1day1ac

---
+++ AtCoderProblems/ABC181A-Heavy_Rotation.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+#define MOD 1000000007
+#define rep(i, n)      for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)     for (int i = 1; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
+template <typename T> bool chmax(T& a, const T& b); //aよりもbが大きいならばaをbで更新する //更新されたならばtrueを返す
+template <typename T> bool chmin(T& a, const T& b); //aよりもbが小さいならばaをbで更新する //更新されたならばtrueを返す
+
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	cout << ((n % 2 == 0) ? "White" : "Black") << endl;
+
+	return 0;
+
+}
+
+
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T& a, const T& b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
