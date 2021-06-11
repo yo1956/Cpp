7dc096ca7527ea5e87f7fd66bf7832b031c9ea56
yo1956 <hm.hr.yossy@gmail.com>
2021-06-11 22:27:43 +0900

    1day1ac

---
+++ ABC179A-Plural_Form.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+#define MOD 1000000007
+#define rep(i, n)      for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)     for (int i = 1; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
+template <typename T> bool chmax(T& a, const T& b); //aよりもbが大きいならばaをbで更新する //更新されたならばtrueを返す
+template <typename T> bool chmin(T& a, const T& b); //aよりもbが小さいならばaをbで更新する //更新されたならばtrueを返す
+
+
+int main() {
+
+	string S;
+	cin >> S;
+
+	string S1(S);
+	// reverse_copy(S.begin(), S.end(), S1);
+	reverse(S1.begin(), S1.end());
+
+	if(S1[0] == 's')
+		cout << (S + "es") << endl;
+	else
+		cout << (S + "s") << endl;
+
+	return 0;
+
+}
+
+
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T& a, const T& b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
