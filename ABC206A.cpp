5327c7bd6ddfae3e67b08dec3d465aacc9299631
yo1956 <hm.hr.yossy@gmail.com>
2021-06-19 23:56:18 +0900

    ABC206

---
+++ ABC206A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+#define MOD 1000000007
+#define rep(i, n)      for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)     for (int i = 1; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
+template <typename T> bool chmax(T& a, const T& b); //aよりもbが大きいならばaをbで更新する //更新されたならばtrueを返す
+template <typename T> bool chmin(T& a, const T& b); //aよりもbが小さいならばaをbで更新する //更新されたならばtrueを返す
+
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int ans = n * 1.08;
+	if(ans < 206){
+		cout << "Yay!" << endl;
+	}
+	else if(ans > 206){
+		cout << ":(" << endl;
+	}
+	else
+		cout << "so-so" << endl;
+
+	return 0;
+
+}
+
+
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T& a, const T& b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
---
+++ lib/memo_map.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    //--------------------------------------------------------------------
+    //mapの走査
+    //コメントアウト部分の記述方法はC++17で使える
+    map<int, ll> mp;
+    int n;
+    ll ans = n*(n-1)/2;
+    //方法1
+	// for(auto&& [a,b]:mp){
+	// 	ans -= b * (b - 1) / 2;
+	// }
+    
+    //方法2
+    map<int, ll>::iterator itr = mp.begin();
+	while(itr != mp.end()){
+	 	ans -= itr->second * (itr->second - 1) / 2; //itr->secondで、keyで回してる時のvalue値を取得できる
+		++itr;
+	}
+
+    //方法3
+    ll ans = 0;
+    vector<int> a(n);
+    for(int i=0; i < n; i++) mp[a[i]]++;
+    //という感じでaがあった場合
+    for (int i = 0; i < n; i++){
+        ans += (n - mp[a[i]]);
+    }
+    //--------------------------------------------------------------------
+}
