4abdfc0f66ecc5711d21684c326e820162f55bc4
yo1956 <hm.hr.yossy@gmail.com>
2021-06-21 21:22:45 +0900

    1day1ac

---
+++ AtCoderProblems/ABC172A_Calc.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+#define MOD 1000000007
+#define rep(i, n)     for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)    for (int i = 1; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
+template <typename T> bool chmax(T& a, const T& b); 
+template <typename T> bool chmin(T& a, const T& b);
+
+
+int main() {
+
+	int a;
+	cin >> a;
+
+	cout << a + a * a + a * a * a << endl;
+
+	return 0;
+
+}
+
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
--- atcoder-template.cpp
+++ atcoder-template.cpp
@@ -3,8 +3,8 @@ using namespace std;
 using Graph = vector<vector<int>>;
 using ll = long long;
 #define MOD 1000000007
-#define rep(i, n)      for (int i = 0; i < (int)(n); ++i)
-#define rep1(i, n)     for (int i = 1; i < (int)(n); ++i)
+#define rep(i, n)     for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)    for (int i = 1; i < (int)(n); ++i)
 #define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
 template <typename T> bool chmax(T& a, const T& b); 
 template <typename T> bool chmin(T& a, const T& b);
