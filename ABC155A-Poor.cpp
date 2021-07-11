33761937b0a1aac53b372739f9dccaa09e030c32
yo1956 <hm.hr.yossy@gmail.com>
2021-07-11 20:36:30 +0900

    1day1ac

---
+++ ABC155A-Poor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+#define MOD 1000000007
+#define rep(i, n)     for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n)    for (int i = 1; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = s; i < (int)(n); ++i)
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+//---------------------------------------------------------------------------------------------------
+
+
+int main() {
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	if((a == b && b == c) || (a != b) && (a != c) && (b != c))
+		cout << "No" << endl;
+	else
+		cout << "Yes" << endl;
+
+	return 0;
+
+}
